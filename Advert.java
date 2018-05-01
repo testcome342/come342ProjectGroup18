f5d08949e41a1c2b8278fea179442be4025d4f73
Talip Can Korkmazer <tkorkmazer@hotmail.com>
2018-05-02 02:54:31 +0300

    Advert class creation

--- Advert.java
+++ Advert.java
@@ -1,16 +1,36 @@
 package com.company;
 
 public class Advert {
+    protected String advertType;
+    private String advertTitle;
+    private String targetCompletionDate;
     private int estimatedAdvertCost;
     private int actualAdvertCost;
 
-    public Advert(int estimatedAdvertCost, int actualAdvertCost) {
+    public Advert(){
+
+    }
+    public Advert(String advertTitle, String targetCompletionDate, int estimatedAdvertCost, int actualAdvertCost) {
+        this.advertTitle = advertTitle;
+        this.targetCompletionDate = targetCompletionDate;
         this.estimatedAdvertCost = estimatedAdvertCost;
         this.actualAdvertCost = actualAdvertCost;
     }
 
     public int getCost(){
         return this.actualAdvertCost;
+    }
+
+    public String getAdvertTitle() {
+        return advertTitle;
+    }
+
+    public String getTargetCompletionDate() {
+        return targetCompletionDate;
+    }
+
+    public String getAdvertType() {
+        return advertType;
     }
 
     public int getEstimatedAdvertCost() {
